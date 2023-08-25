dbfbf4ad3bf5378dbc508aa3542332188b0c452e
Matthew Morrison <mmorri22@nd.edu>
2023-08-25 19:41:00 -0400

    Move to CSE 30342 Resources for Cadence Setup

---
+++ Resources/Cadence/cdssetup.sh
@@ -0,0 +1,15 @@
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/Resources/Cadence/cds.lib
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/Resources/Cadence/cdsLibMgr.il
+wget hhttps://raw.githubusercontent.com/mmorri22/cse30342/main/Resources/Cadence/cdsenv
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/Resources/Cadence/cdsinfo.tag
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/Resources/Cadence/cdsinit
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/Resources/Cadence/common_bindkeys.il
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/Resources/Cadence/display.drf
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/Resources/Cadence/layoutPlotTemplate
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/Resources/Cadence/lib.defs
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/Resources/Cadence/merge_display.pl
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/Resources/Cadence/schPlotTemplate
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/Resources/Cadence/simrc
+mv cdsinit .cdsinit
+mv simrc .simrc
+mv cdsenv .cdsenv
