f2416e6a4a852d6f1a257794f3ed12638c9005bb
Matthew Morrison <mmorri22@nd.edu>
2023-12-01 11:38:22 -0500

    Setup Script

---
+++ lec38/risc_setup.sh
@@ -0,0 +1,9 @@
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/lec38/multiplexor.v
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/lec38/driver.v
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/lec38/alu.v
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/lec38/controller.v
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/lec38/memory.v
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/lec38/register.v
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/lec38/counter.v
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/lec38/risc.v
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/lec38/risc_test.v
