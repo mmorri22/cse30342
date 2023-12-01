a45d9525a98c09d831b3e23438f9d92ac671e618
Matthew Morrison <mmorri22@nd.edu>
2023-12-01 11:40:09 -0500

    Lecture 38 Setup Fix

--- lec38/risc_setup.sh
+++ lec38/risc_setup.sh
@@ -7,3 +7,6 @@ wget https://raw.githubusercontent.com/mmorri22/cse30342/main/lec38/register.v
 wget https://raw.githubusercontent.com/mmorri22/cse30342/main/lec38/counter.v
 wget https://raw.githubusercontent.com/mmorri22/cse30342/main/lec38/risc.v
 wget https://raw.githubusercontent.com/mmorri22/cse30342/main/lec38/risc_test.v
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/lec38/CPUtest1.txt
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/lec38/CPUtest2.txt
+wget https://raw.githubusercontent.com/mmorri22/cse30342/main/lec38/CPUtest3.txt
